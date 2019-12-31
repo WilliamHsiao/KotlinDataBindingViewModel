4c2ebc53afc937fac5c18a51dbdc21280d2774c1
WilliamHsiao <%V3e%Am%gPVxjs>
2019-12-31 22:28:23 +0800

    databinding版本升級

--- app/src/main/java/com/example/kotlindatabindingviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/kotlindatabindingviewmodel/MainActivity.kt
@@ -2,23 +2,18 @@ package com.example.kotlindatabindingviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.kotlindatabindingviewmodel.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
-import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
-    lateinit var viewModel: MainViewModel
-    lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main).apply {
-            this.lifecycleOwner=this@MainActivity
-            this.viewModel=this@MainActivity.viewModel
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = viewModel
 
     }
 }
